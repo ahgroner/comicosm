fcce6ebce6dbbc6a64d73bf5ce70b790e1f2a1e5
ahgroner <adamhgroner@gmail.com>
2025-03-17 17:19:33 -0700

    font!

--- src/CharacterList.tsx
+++ src/CharacterList.tsx
@@ -1,7 +1,15 @@
+import React from "react";
 import { characters } from "./characters";
-import { ListItem, ListItemText, Stack } from "@mui/material";
-
+import { ListItem, ListItemText, Stack, TextField } from "@mui/material";
 export const CharacterList = () => {
+  const [search, setSearch] = React.useState("");
+
+  const filteredCharacters = search
+    ? characters.filter((character) =>
+        character.toLowerCase().includes(search.toLowerCase())
+      )
+    : characters;
+
   return (
     <Stack
       sx={{
@@ -13,8 +21,30 @@ export const CharacterList = () => {
         overflow: "auto",
       }}
     >
-      {characters.map((character) => (
-        <ListItem key={character}>
+      <Stack
+        sx={{
+          position: "sticky",
+          top: 0,
+          backgroundColor: "white",
+          zIndex: 1000,
+        }}
+      >
+        {/* <Typography variant="h6">Characters</Typography> */}
+        <TextField
+          size="small"
+          sx={{
+            ".MuiInputBase-root": {
+              borderRadius: 1000,
+            },
+            width: "100%",
+            padding: 1,
+          }}
+          placeholder="Find a character"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Stack>
+      {filteredCharacters.map((character) => (
+        <ListItem key={character} sx={{ height: 24 }}>
           <ListItemText primary={character} />
         </ListItem>
       ))}
--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,4 +1,30 @@
 import { createTheme, ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'var(--font-sour-gummy)',
+    allVariants: {
+      fontFamily: 'var(--font-sour-gummy)',
+    },
+  },
+  components: {
+    MuiListItem: {
+      styleOverrides: {
+        root: {
+          fontFamily: 'var(--font-sour-gummy)',
+        },
+      },
+    },
+    MuiListItemText: {
+      styleOverrides: {
+        primary: {
+          fontFamily: 'var(--font-sour-gummy)',
+        },
+      },
+    },
+  },
+});
+
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  return <MuiThemeProvider theme={createTheme()}>{children}</MuiThemeProvider>;
+  return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
 };
